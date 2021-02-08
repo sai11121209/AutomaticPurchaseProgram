471ca4ff8bc42b07cbf48cdada10ae826755694d
sai11121209 <sai11121209@gmail.com>
2021-02-08 20:17:12 +0900

    #09 処理変更

--- TokyoDisneyResort/TicketStatus/LineAPI.py
+++ TokyoDisneyResort/TicketStatus/LineAPI.py
@@ -22,15 +22,15 @@ def send_message(access_token, Datas):
     message = "再販あり\n"
     for Key in Datas.keys():
         if len(Datas[Key]):
-            message += f"東京ディズニー{Key}\n"
+            message += f"東京ディズニー{Key}: {len(Datas[Key])}件\n"
         for Data in Datas[Key]:
             message += f"{Data}\n"
     payload = {"message": message}
     rq.post(url, headers=headers, params=payload)
-    message = "以下のURLに記載されて居る日本語の部分を数値に置き換えてサイトに飛ぶことで直接移動できます。"
+    message = "以下のURLに記載されて居る日本語の部分(2項目)を数値に置き換えてサイトに飛ぶことで直接移動できます。"
     payload = {"message": message}
     rq.post(url, headers=headers, params=payload)
-    message = "https://reserve.tokyodisneyresort.jp/sp/ticket/search/?parkTicketGroupCd=01&route=2&selectParkDay1=ランドなら01シーなら02&useDays=1&numOfJunior=中人の人数&useDateFrom=年(西暦)月(2桁)日(2桁)&parkTicketSalesForm=1&numOfAdult=大人の人数&numOfChild=小人の人数"
+    message = "https://reserve.tokyodisneyresort.jp/sp/ticket/search/?parkTicketGroupCd=01&route=2&selectParkDay1=ランドなら01シーなら02&useDays=1&useDateFrom=年(西暦)月(2桁)日(2桁)&parkTicketSalesForm=1"
     payload = {"message": message}
     rq.post(url, headers=headers, params=payload)
 
